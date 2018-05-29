9fe0a979e5494481ebbe9691311ba4eb54731356
jrubin13 <jrubin@stuy.edu>
2018-05-29 14:50:44 -0400

    began working

---
+++ VideoGui.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class VideoGui extends JFrame implements ActionListener{
+    private JPanel buttonPane;
+    private JPanel videoPane;
+    
+}
